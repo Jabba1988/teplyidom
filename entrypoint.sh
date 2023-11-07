0c737965591e24559b5fea7716f6780a30b49b0d
Ivan Bakhmatov <jabba@list.ru>
2023-11-07 18:34:52 +0500

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-python /app/manage.py collectstatic --noinput
-python /app/manage.py makemigrations --noinput
+#python /app/manage.py collectstatic --noinput
+#python /app/manage.py makemigrations --noinput
 
 python /app/manage.py runserver 0.0.0.0:8000
